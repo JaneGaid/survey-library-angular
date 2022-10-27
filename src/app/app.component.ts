1d2339bf60aeb8ff89d1efe43d79043285fe66bc
Jane <zhanna.gaidukova@gmail.com>
2022-10-27 12:26:23 +0300

    New Example

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
+import { SurveyModel, StylesManager } from "survey-core";
 
+StylesManager.applyTheme("defaultV2");
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"]
 })
 export class AppComponent {
-  title = 'survey-library-angular';
+  model = new SurveyModel({
+  elements: [
+  {
+    name: "name",
+    type: "text",
+    title: "Please enter your name:",
+    placeHolder: "Jon Snow",
+    isRequired: true,
+   }
+  ]
+});
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,15 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppComponent } from './app.component';
+
+import { SurveyAngularModule } from "survey-angular-ui";
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule, SurveyAngularModule
   ],
   providers: [],
   bootstrap: [AppComponent]
